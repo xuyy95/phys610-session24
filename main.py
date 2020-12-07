4282fb726e23bc1f3ae03f83b9f5d6dc160c3a44
Yuanyuan Xu <yuanyuan.xu@jhu.edu>
2020-12-06 23:56:12 -0500

    Add part a
    Visualization of trajectories for cannonballs of different masses

--- main.py
+++ main.py
@@ -3,9 +3,34 @@ import matplotlib.pyplot as plt
 from calculator import Cannonball
 
 
-ball = Cannonball(planet='Earth', m=1.0, C=0.47, R=0.08, theta=30.0, v0=100)
-x, y = ball.RK()
+# plot setup
+plt.rc('font',**{'family':'sans-serif','sans-serif':['DejaVu Sans']}) # sans-serif
+plt.rc('mathtext',**{'default':'regular'})
+plt.rc('xtick', labelsize=12, direction='in')
+plt.rc('ytick', labelsize=12, direction='in')
+plt.rc('axes', labelsize=14)
 
-plt.plot(x, y)
+plt.rcParams['figure.constrained_layout.use'] = True
+plt.rcParams['axes.linewidth'] = 1
+plt.rcParams['xtick.major.width'] = 1
+plt.rcParams['xtick.major.size'] = 6
+plt.rcParams['ytick.major.width'] = 1
+plt.rcParams['ytick.major.size'] = 6
 
-plt.show()
+
+# part a
+if False:
+	fig, ax = plt.subplots(figsize=(6,4.5))
+
+	planet = 'Mars'
+
+	for m in [0.25,0.5,1,2,4]:
+		ball = Cannonball(planet=planet, m=m, C=0.47, R=0.08, theta=30.0, v0=100)
+		x, y = ball.RK()
+		ax.plot(x, y, label='m = '+str(m)+' kg')
+		ax.set_xlabel("x [m]")
+		ax.set_ylabel('y [m]')
+		ax.legend(loc='upper left', frameon=False, prop={'size':12})
+
+	plt.savefig('../plot/{0}_part_a.pdf'.format(planet), transparent=True)
+	plt.show()
